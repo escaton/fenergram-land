b38632d60a4a154b347263598fa5b577ad84d1a9
Dmitry Vasilyev <vaseker@yandex-team.ru>
2015-09-26 19:55:47 +0300

    Deploy fix

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,4 +4,4 @@ rsync -avzh ./ fenegram@fenegram.com:/opt/fenegram/app \
     --exclude static/bower_components \
     --exclude server/node_modules \
     --exclude node_modules;
-ssh fenegram.com "cd /opt/fenegram/app/server && npm i && pm2 restart fenegram"
+ssh fenegram@fenegram.com "cd /opt/fenegram/app/server && npm i && pm2 restart fenegram"
