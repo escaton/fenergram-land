2217c155081c7552829a573b72fb5d2e2b7b5e09
Dmitry Vasilyev <vaseker@yandex-team.ru>
2015-09-26 16:03:38 +0300

    Merge branch 'master' of github.com:vaseker/fenergram-land
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- static/js/lib/scroll.js
+++ static/js/lib/scroll.js
@@ -1,20 +1,22 @@
 require('owl.carousel');
 
 module.exports = function (scroll) {
-    scroll.owlCarousel({
-        items: 1,
-        loop: true,
-        autoWidth: false,
-        nav: false,
-        navText: [],
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 3000
-    });
     var onDragOnce = function () {
         scroll.trigger('stop.owl.autoplay');
         scroll.off('dragged.owl.carousel', onDragOnce);
     };
-    scroll.on('dragged.owl.carousel', onDragOnce);
+    setTimeout(function () {
+        scroll.owlCarousel({
+            items: 1,
+            loop: true,
+            autoWidth: true,
+            nav: false,
+            navText: [],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 3000
+        });
+        scroll.on('dragged.owl.carousel', onDragOnce);
+    }, 0);
     return scroll;
 };
