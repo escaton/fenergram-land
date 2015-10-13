2c51329e31cf89b6057af640b9bd70b716ceded5
Nikita Popov <losnikitos@yandex-team.ru>
2015-10-13 13:43:48 +0300

    Content

--- static/js/ios.boarding.entry.js
+++ static/js/ios.boarding.entry.js
@@ -6,13 +6,13 @@ $ = require('jquery');
 var scroller = require('./lib/scroll');
 
 $(document).ready(function () {
-    var scroll = scroller($('.scroll'));
+    var scroll = scroller($('.scroll'), { dotsContainer: '#slide-dots' });
     scroll.on('changed.owl.carousel', function (event) {
         var index = event.item.index || 0;
         var slide = $('.scroll__elem').eq(index);
         var title = slide.data('title');
 
-        //$('.teaser__title').text(slide.data('title'));
+        $('.teaser__title').text(slide.data('title'));
         $('.teaser__text').text(slide.data('subtitle'));
     })
 });
