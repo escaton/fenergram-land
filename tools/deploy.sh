025fd57f4452d1ea55f09f35db41c6d4bce5a94f
Egor Blinov <escaton@yandex-team.ru>
2015-10-13 11:19:03 +0300

    non fenegram user in deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,6 @@
-rsync -avzh ./ fenegram@fenegram.com:/opt/fenegram/app \
+rsync -avzh ./ fenegram.com:/opt/fenegram/app \
     --exclude .git \
     --exclude static/node_modules \
     --exclude static/bower_components \
-    --exclude server/node_modules \
     --exclude node_modules;
-ssh fenegram@fenegram.com "cd /opt/fenegram/app/server && npm i && pm2 restart fenegram"
+ssh fenegram.com "pm2 restart fenegram"
